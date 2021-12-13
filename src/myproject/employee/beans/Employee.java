31a91668d884973e26ae7be58e4a0f6562fc3542
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2021-12-14 00:45:40 +0530

    Employee Bean Class Created

---
+++ src/myproject/employee/beans/Employee.java
@@ -0,0 +1,42 @@
+package myproject.employee.beans;
+
+public class Employee {
+
+	private int eno;
+	private String ename;
+	private float esal;
+	private String eaddr;
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public float getEsal() {
+		return esal;
+	}
+	public void setEsal(float esal) {
+		this.esal = esal;
+	}
+	public String getEaddr() {
+		return eaddr;
+	}
+	public void setEaddr(String eaddr) {
+		this.eaddr = eaddr;
+	}
+	
+	public void getEmpDetails() {
+		System.out.println("Employee Details");
+		System.out.println("--------------------------------");
+		System.out.println("Employee Number 	:"+eno);
+		System.out.println("Employee Name		:"+ename);
+		System.out.println("Employee Salary 	:"+esal);
+		System.out.println("Employee Address	:"+eaddr);
+	}
+}
